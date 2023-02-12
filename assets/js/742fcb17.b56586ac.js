40b4c20f01109e57edde6cf58b05d5db6a45e038
CodedJimmy <46508035+CodedJimmy@users.noreply.github.com>
2023-02-12 10:56:52 -0500

    Built and published by Moonwave

---
+++ assets/js/742fcb17.b56586ac.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[930],{7636:e=>{e.exports=JSON.parse('[{"type":"link","href":"/api/Error","label":"\u200bError"},{"type":"link","href":"/api/Promise","label":"\u200bPromise"},{"type":"link","href":"/api/TGFrameworkServer","label":"\u200bT\u200bG\u200bFramework\u200bServer"}]')}}]);
---
+++ assets/js/f08a52a5.ebc80a89.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[487],{63891:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/TrizaCorporation/TGFramework/blob/master","baseUrl":"/","classOrder":[],"apiCategories":[]}')}}]);
